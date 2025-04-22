e8036f9f4215ac1a70cc053d4437c16b6fb4ebfd
MariaValeria-Sandru <valy895@yahoo.com>
2025-04-22 12:42:52 +0200

    observateur d'eveniment

---
+++ Exercice4/index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("myButton");
+
+    button.addEventListener("click", () => {
+        const message = document.createElement("p");
+        message.textContent = "Bonjour, vous avez cliqué sur le bouton !";
+        document.body.appendChild(message);
+    });
+});
